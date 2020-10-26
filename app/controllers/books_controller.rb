b7c1cf93a19b9ed5ef8841ee3343d97e7429a765
EC2 Default User <ec2-user@ip-172-31-45-58.ec2.internal>
2020-10-26 07:46:52 +0000

    [add]修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+  end
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -10,6 +14,32 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    # bookのユーザidとログイン中のidが一致した時のみ編集可能
+    if @book.user_id == current_user.id
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.user_id == current_user.id
+      book.destroy
+      redirect_to books_path
+    else
+      redirect_to books_path
+    end
   end
 
   private
